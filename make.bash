61be096c50aa3903144e4c3c901696061386e990
Ben Lubar <ben.lubar@gmail.com>
2018-04-23 19:06:37 -0500

    Now that we aren't using a single container for this, we don't need the hacky version of PATH.

--- make.bash
+++ make.bash
@@ -1,4 +1,6 @@
 #!/bin/bash -ex
 
 docker build --pull -t proget.local.lubar.me/dfhack-docker/build-env:gcc-4.8 gcc48
+docker push proget.local.lubar.me/dfhack-docker/build-env:gcc-4.8
 docker build --pull -t proget.local.lubar.me/dfhack-docker/build-env:latest gcc7
+docker push proget.local.lubar.me/dfhack-docker/build-env:latest

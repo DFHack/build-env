4e21d667f267a82cc03f8044f3676f04d11993ec
Ben Lubar <ben.lubar@gmail.com>
2022-05-15 01:38:52 -0500

    fix boostcpp download url

--- make.bash
+++ make.bash
@@ -1,9 +1,11 @@
 #!/bin/bash -ex
 
+#nc=--no-cache
+
 tag=proget.lubar.me/dfhack-docker/build-env
-docker build --pull -t $tag:gcc-4.8 gcc48
+docker build $nc --pull -t $tag:gcc-4.8 gcc48
 docker push $tag:gcc-4.8
-docker build --pull -t $tag:latest gcc7
+docker build $nc --pull -t $tag:latest gcc7
 docker push $tag:latest
-docker build --pull -t $tag:msvc msvc
+docker build $nc --pull -t $tag:msvc msvc
 docker push $tag:msvc
